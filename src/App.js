a5f1c904c76298b242092e9d1c100f15c11ccb03
Mariam <Maf9894@gmail.com>
2023-05-28 19:53:32 +0300

    task 2 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
 import image from "./assets/images/image1.jpeg";
+import products from "./products";
 function App() {
+  const productList = products.map((product) => {
+    return (
+      <div className="main">
+        <img src={product.image} className="iamgeStyle" />
+        <h1>
+          {product.name}: {product.price}
+        </h1>
+      </div>
+    );
+  });
   return (
     <>
       <div className="main">
         <div className="style">
           <h1>Maryam's Bakery</h1>
-          <h4>The taste of home-baked goodness</h4>
+          <h3>The taste of home-baked goodness</h3>
+          <img src={image}></img>
+
+          {productList}
         </div>
-        <img src={image}></img>
       </div>
     </>
   );
---
+++ src/products.js
@@ -0,0 +1,34 @@
+import image1 from "./assets/images/muffins.png";
+import image2 from "./assets/images/brownies.png";
+import image3 from "./assets/images/cookies.png";
+import image4 from "./assets/images/tiramisu.png";
+import image5 from "./assets/images/croisant.png";
+const products = [
+  {
+    name: "Muffins",
+    price: "2 KD",
+    image: image1,
+  },
+  {
+    name: "Brownies",
+    price: "1.5 KD",
+    image: image2,
+  },
+  {
+    name: "Cookies",
+    price: "1.5 KD",
+    image: image3,
+  },
+  {
+    name: "Tiramisu",
+    price: "2 KD",
+    image: image4,
+  },
+  {
+    name: "Croissant",
+    price: "2 KD",
+    image: image5,
+  },
+];
+
+export default products;
